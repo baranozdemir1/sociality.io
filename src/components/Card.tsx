1b39e0a752d09ced118c930c22917de4156c1801
Baran Özdemir <baranozdemir305@gmail.com>
2022-08-30 15:12:07 +0300

    icons fixed

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,5 @@
 import React from 'react'
 
-import { GoComment } from 'react-icons/go'
-import { AiOutlineLike, AiOutlineShareAlt, AiOutlineEye, AiOutlineHeart } from 'react-icons/ai'
-import { IoBanOutline, IoTrashOutline } from 'react-icons/io5'
-import { CgMoreO } from 'react-icons/cg'
-import { TiTick } from 'react-icons/ti'
-import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa'
 import { IDateType } from '../types'
 
 import NoPostImage from '../assets/no-post-image.png'
@@ -17,11 +11,54 @@ interface ICardProps {
 }
 
 const Card: React.FC<ICardProps> = (props) => {
+  const TickIcon: React.FC = () => (
+    <svg className='w-5 h-5 text-[#d5d5d5]' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+    </svg>
+  )
+
+  const BanIcon: React.FC = () => (
+    <svg className='w-5 h-5 text-[#d5d5d5]' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+    </svg>
+  )
+
+  const FacebookIcon: React.FC = () => (
+    <svg className="text-white w-6 h-6" stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 320 512" xmlns="http://www.w3.org/2000/svg">
+      <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
+    </svg>
+  )
+
+  const TwitterIcon: React.FC = () => (
+    <svg className="text-white w-6 h-6" stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+      <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" />
+    </svg>
+  )
+
+  const InstagramIcon: React.FC = () => (
+    <svg className="text-white w-6 h-6" stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg">
+      <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+    </svg>
+  )
+
+  const LikeIcon: React.FC = () => (
+    <svg className="w-4 h-4 text-brand-deepGray" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z" />
+    </svg>
+  )
+
+  const HeartIcon: React.FC = () => (
+    <svg className="w-4 h-4 text-brand-deepGray" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+    </svg>
+  )
+
   const { item } = props
+
   return (
     <div className='card bg-white flex rounded-md overflow-hidden border border-[#e5e6ee]'>
       <div className={`w-10 h-full flex items-center justify-center ${item.status === 0 ? 'bg-brand-needApproval' : item.status === 1 ? 'bg-brand-scheduled' : item.status === 2 ? 'bg-brand-notes' : item.status === 3 ? 'bg-brand-published' : item.status === 4 ? 'bg-brand-error' : ''}`}>
-        {item.account.channel === 'facebook' ? <FaFacebookF size={24} className="text-white" /> : item.account.channel === 'instagrambusiness' ? <FaInstagram size={24} className="text-white" /> : item.account.channel === 'twitter' ? <FaTwitter size={24} className="text-white" /> : ''}
+        {item.account.channel === 'facebook' ? <FacebookIcon /> : item.account.channel === 'instagrambusiness' ? <InstagramIcon /> : item.account.channel === 'twitter' ? <TwitterIcon /> : ''}
       </div>
       <div className='flex-1 px-5 py-6 gap-5 flex flex-wrap flex-col'>
         <div className='flex items-center justify-between'>
@@ -30,13 +67,17 @@ const Card: React.FC<ICardProps> = (props) => {
           </div>
           <ul className='buttons flex flex-wrap items-center justify-center gap-2'>
             <li>
-              {item.status === 0 ? <TiTick size={18} className="text-[#d5d5d5]" /> : item.status === 1 ? <IoBanOutline size={18} className="text-[#d5d5d5]" /> : ''}
+              {item.status === 0 ? <TickIcon /> : item.status === 1 ? <BanIcon /> : ''}
             </li>
             <li>
-              <IoTrashOutline size={18} className="text-[#d5d5d5]" />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-[#d5d5d5]">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+              </svg>
             </li>
             <li>
-              <CgMoreO size={18} className="text-[#d5d5d5]" />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-[#d5d5d5]">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              </svg>
             </li>
           </ul>
         </div>
@@ -57,19 +98,26 @@ const Card: React.FC<ICardProps> = (props) => {
           />
           <ul className='flex items-center justify-start gap-2'>
             <li className='flex items-center justify-center gap-1'>
-              {item.account.channel === 'twitter' ? <AiOutlineHeart size={17} className="text-brand-deepGray" /> : <AiOutlineLike size={17} className="text-brand-deepGray" />}
+              {item.account.channel === 'twitter' ? <HeartIcon /> : <LikeIcon />}
               <span className="text-brand-deepGray">0</span>
             </li>
             <li className='flex items-center justify-center gap-1'>
-              <GoComment size={17} className="text-brand-deepGray" />
+              <svg className="w-4 h-4 text-brand-deepGray" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+              </svg>
               <span className="text-brand-deepGray">0</span>
             </li>
             <li className='flex items-center justify-center gap-1'>
-              <AiOutlineShareAlt size={17} className="text-brand-deepGray" />
+              <svg className="w-4 h-4 text-brand-deepGray" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
+              </svg>
               <span className="text-brand-deepGray">0</span>
             </li>
             <li className='flex items-center justify-center gap-1'>
-              <AiOutlineEye size={17} className="text-brand-deepGray" />
+              <svg className="w-4 h-4 text-brand-deepGray" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+              </svg>
               <span className="text-brand-deepGray">0</span>
             </li>
           </ul>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,19 +7,18 @@ import Logo4 from '../assets/logo-4.png'
 import Logo5 from '../assets/logo-5.png'
 import Logo6 from '../assets/logo-6.png'
 
-import { MdOutlineNotificationsActive } from 'react-icons/md'
-import { AiOutlineFundProjectionScreen, AiOutlineForm, AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
-import { FiActivity } from 'react-icons/fi'
-import { FaRegComments } from 'react-icons/fa'
-import { BsGraphUp } from 'react-icons/bs'
-
 interface ILayoutProps {
   children: JSX.Element | JSX.Element[]
+}
+
+interface ISVGIconProps {
+  className: string
 }
 
 const Layout: React.FC<ILayoutProps> = (props: ILayoutProps) => {
   const [brandActive, setBrandActive] = useState<Number | null>(null)
   const [isMenuActive, setIsMenuActive] = useState<Number | null>(null)
+
   const brands = [
     {
       name: '100% Organic',
@@ -34,30 +33,86 @@ const Layout: React.FC<ILayoutProps> = (props: ILayoutProps) => {
     {
       name: '100% Organic',
       logo: Logo3,
-      notification: 99
+      notification: 0
     },
     {
       name: '100% Organic',
       logo: Logo4,
-      notification: 99
+      notification: 0
     },
     {
       name: '100% Organic',
       logo: Logo5,
-      notification: 99
+      notification: 0
     },
     {
       name: '100% Organic',
       logo: Logo6,
-      notification: 99
+      notification: 0
     }
   ]
+
+  const SummaryIcon: React.FC<ISVGIconProps> = (props) => {
+    return (
+      <svg className={`${props.className} h-5 w-5`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" >
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605" />
+      </svg>
+    )
+  }
+
+  const PublishIcon: React.FC<ISVGIconProps> = (props) => {
+    return (
+      <svg className={`${props.className} h-5 w-5`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+      </svg>
+    )
+  }
+
+  const EngageIcon: React.FC<ISVGIconProps> = (props) => {
+    return (
+      <svg className={`${props.className} h-5 w-5`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
+      </svg>
+    )
+  }
+
+  const ListenIcon: React.FC<ISVGIconProps> = (props) => {
+    return (
+      <svg className={`${props.className} h-5 w-5`} stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" xmlns="http://www.w3.org/2000/svg">
+        <polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline>
+      </svg>
+    )
+  }
+
+  const ReportIcon: React.FC<ISVGIconProps> = (props) => {
+    return (
+      <svg className={`${props.className} h-5 w-5`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
+      </svg>
+    )
+  }
+
+  const PlusIcon: React.FC<ISVGIconProps> = (props) => {
+    return (
+      <svg className={`${props.className} h-5 w-5`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+      </svg>
+    )
+  }
+
+  const MinusIcon: React.FC<ISVGIconProps> = (props) => {
+    return (
+      <svg className={`${props.className} h-5 w-5`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15" />
+      </svg>
+    )
+  }
 
   const menuItems = [
     {
       id: 0,
       name: 'SUMMARY',
-      icon: <AiOutlineFundProjectionScreen size={20} className={isMenuActive === 0 ? 'text-white' : 'text-brand-default'} />,
+      icon: <SummaryIcon className={isMenuActive === 0 ? 'text-white' : 'text-brand-default'} />,
       subMenu: [
         {
           name: 'Compose',
@@ -72,7 +127,7 @@ const Layout: React.FC<ILayoutProps> = (props: ILayoutProps) => {
     {
       id: 1,
       name: 'PUBLISH',
-      icon: <AiOutlineForm size={20} className={isMenuActive === 1 ? 'text-white' : 'text-brand-default'} />,
+      icon: <PublishIcon className={isMenuActive === 1 ? 'text-white' : 'text-brand-default'} />,
       subMenu: [
         {
           name: 'Compose',
@@ -87,7 +142,7 @@ const Layout: React.FC<ILayoutProps> = (props: ILayoutProps) => {
     {
       id: 2,
       name: 'ENGAGE',
-      icon: <FaRegComments size={20} className={isMenuActive === 2 ? 'text-white' : 'text-brand-default'} />,
+      icon: <EngageIcon className={isMenuActive === 2 ? 'text-white' : 'text-brand-default'} />,
       subMenu: [
         {
           name: 'Compose',
@@ -102,7 +157,7 @@ const Layout: React.FC<ILayoutProps> = (props: ILayoutProps) => {
     {
       id: 3,
       name: 'LISTEN',
-      icon: <FiActivity size={20} className={isMenuActive === 3 ? 'text-white' : 'text-brand-default'} />,
+      icon: <ListenIcon className={isMenuActive === 3 ? 'text-white' : 'text-brand-default'} />,
       subMenu: [
         {
           name: 'Compose',
@@ -117,7 +172,7 @@ const Layout: React.FC<ILayoutProps> = (props: ILayoutProps) => {
     {
       id: 4,
       name: 'REPORT',
-      icon: <BsGraphUp size={20} className={isMenuActive === 4 ? 'text-white' : 'text-brand-default'} />,
+      icon: <ReportIcon className={isMenuActive === 4 ? 'text-white' : 'text-brand-default'} />,
       subMenu: [
         {
           name: 'Compose',
@@ -144,7 +199,7 @@ const Layout: React.FC<ILayoutProps> = (props: ILayoutProps) => {
                 brands.map((brand: any, index: number) => (
                   <div key={index} className={`brand relative pt-2 cursor-pointer flex ${brandActive === index ? 'brandActive' : ''}`} onClick={() => setBrandActive(index)}>
                     <img src={brand.logo} alt={brand.name} width={37} height={37} className="rounded-lg ml-2" />
-                    <span className='absolute top-0 right-2 bg-[#3ac1a9] h-[18px] w-[18px] text-[10px] text-center rounded-full text-white flex items-center justify-center'>{brand.notification}</span>
+                    {brand.notification > 0 && <span className='absolute top-0 right-2 bg-[#3ac1a9] h-[18px] w-[18px] text-[10px] text-center rounded-full text-white flex items-center justify-center'>{brand.notification}</span>}
                   </div>
                 ))
               }
@@ -154,7 +209,9 @@ const Layout: React.FC<ILayoutProps> = (props: ILayoutProps) => {
                 <li>
                   <a className='flex justify-between items-center p-3'>
                     <div className='flex items-center justify-center gap-3'>
-                      <MdOutlineNotificationsActive size={20} className='text-brand-default' />
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-brand-default">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
+                      </svg>
                       <span className='text-white text-xs col-span-2'>NOTIFICATIONS</span>
                     </div>
                     <span className='text-white text-xs border border-[#73d9c7] py-2 px-4 h-0 w-0 rounded-md flex items-center justify-center'>29</span>
@@ -168,7 +225,7 @@ const Layout: React.FC<ILayoutProps> = (props: ILayoutProps) => {
                         {item.icon}
                         <span className='text-white text-xs'>{item.name}</span>
                       </div>
-                      {isMenuActive !== item.id && item.subMenu !== undefined ? <AiOutlinePlus size={20} className={`${isMenuActive === item.id ? 'text-white' : 'text-black'}`} /> : <AiOutlineMinus size={20} className={`${isMenuActive === item.id ? 'text-white' : 'text-black'}`} />}
+                      {isMenuActive !== item.id && item.subMenu !== undefined ? <PlusIcon className={`${isMenuActive === item.id ? 'text-white' : 'text-black'}`} /> : <MinusIcon className={`${isMenuActive === item.id ? 'text-white' : 'text-black'}`} />}
                     </div>
                     {item.subMenu !== undefined && isMenuActive === item.id && (
                       <ul className='bg-[#32363a] px-8 list-disc'>
